bb0a858a972a30b0e85561654d2dd235e3e9227c
mu-nss <msj20161282@gmail.com>
2024-12-31 13:25:30 +0900

    first commit

---
+++ src/main/java/com/zerobase/store_reservation_api/StoreReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.store_reservation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/store_reservation_api/StoreReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.store_reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
