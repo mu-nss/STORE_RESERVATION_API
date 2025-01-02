e4a6ffc7425cb7b5d0b7521f2ec07477e63f74d7
mu-nss <msj20161282@gmail.com>
2025-01-02 14:20:48 +0900

    [PasswordEncoder]-Spring Security:패스워드 인코딩

---
+++ src/main/java/com/zerobase/store_reservation_api/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.zerobase.store_reservation_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    /** Spring Security - 패스워드 인코딩 **/
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
